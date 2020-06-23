7d06383da60d7d5cf0c5fa08eab33ad49c05cbc8
Erick Kurniawan <erick.kurniawan@gmail.com>
2020-06-23 11:45:33 +0700

    menambahkan custom list

---
+++ LatihanXamarinApp/LatihanXamarinApp/ContohCustomList.xaml.cs
@@ -0,0 +1,33 @@
+﻿using LatihanXamarinApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LatihanXamarinApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ContohCustomList : ContentPage
+    {
+        private List<ListItem> lstItems;
+        public ContohCustomList()
+        {
+            InitializeComponent();
+            lstItems = new List<ListItem>
+            {
+                new ListItem{Title="Xamarin Android",Price="12",
+                    Description="Xamarin with Traditional Android UI",Source="monkey.png"},
+                new ListItem{Title="Xamarin IOS",Price="20",
+                    Description="Xamarin with Traditional IOS UI",Source="monkey2.png"},
+                new ListItem{Title="Xamarin Forms",Price="30",
+                    Description="Cross Platform Xamarin Forms",Source="monkey3.png"}
+            };
+
+            lvImage.ItemsSource = lstItems;
+        }
+    }
+}
--- LatihanXamarinApp/LatihanXamarinApp/Models/ListItem.cs
+++ LatihanXamarinApp/LatihanXamarinApp/Models/ListItem.cs
@@ -9,5 +9,6 @@ namespace LatihanXamarinApp.Models
         public string Title { get; set; }
         public string Description { get; set; }
         public string Source { get; set; }
+        public string Price { get; set; }
     }
 }
